aed4a74eb1591a40bb5ecd78118543bb25dd46cb
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-04-11 13:19:52 -0400

    server is working locally

--- server.js
+++ server.js
@@ -2,3 +2,29 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.use(express.json());
+
+// console.log("test")
+
+app.get('/', (req, res) => {
+    console.log('Received request on /');
+    res.send('Chat app backend is running');
+    
+});
+
+io.on('connection', socket => {
+    console.log('New WebSocket connection');
+
+    socket.on('disconnect', () => {
+        console.log('User has disconnected');
+    });
+});
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
