d8f74c9708ca816f8b0f165ceb6ebd8b7769313b
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-04-15 21:53:30 -0400

    added firestore

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+// const express = require('express');
+// const http = require('http');
+// const socketio = require('socket.io');
+// // local host
+// const app = express();
+// const server = http.createServer(app);
 
-const app = express();
-const server = http.createServer(app);
+// const io = socketio(server);
 
-const io = socketio(server);
+// app.use(express.json());
 
-app.use(express.json());
+// // console.log("test")
 
-// console.log("test")
-
-app.get('/', (req, res) => {
-    console.log('Received request on /');
-    res.send('Chat app backend is running');
+// app.get('/', (req, res) => {
+//     console.log('Received request on /');
+//     res.send('Chat app backend is running');
     
+// });
+
+// io.on('connection', socket => {
+//     console.log('New WebSocket connection');
+
+//     socket.on('disconnect', () => {
+//         console.log('User has disconnected');
+//     });
+// });
+
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const admin = require('firebase-admin');
+const serviceAccount = require('c:/Users/banan/Documents/GitHub/database manager/database-chatting-app-dc0895893908.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
 
-io.on('connection', socket => {
-    console.log('New WebSocket connection');
+const db = admin.firestore();
 
-    socket.on('disconnect', () => {
-        console.log('User has disconnected');
-    });
-});
-
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
