f948dd4dd4d02fe17309add9fde4ba2196b47124
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-04-21 22:29:41 -0400

    attempting to fix docker desktop 'MODULE_NOT_FOUND', error

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,6 @@
 // /config/firebaseConfig.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../../database manager/database-chatting-app-dc0895893908.json');  // Adjust the path as necessary
+const serviceAccount = require('../database-chatting-app-dc0895893908.json');  // Adjust the path as necessary
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
