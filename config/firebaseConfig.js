88bc49e96b9f69e49af8da083dae746f6183afd5
Max Nickerson <41595474+MaxNickerson@users.noreply.github.com>
2024-04-23 11:33:53 -0400

    docker compose up and running

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 // Path to your Firebase service account key file
 // Make sure to replace 'path-to-your-service-account-key.json' with the correct path
-const serviceAccount = require('../database-chatting-app-a928494373d5.json.json');
+const serviceAccount = require('../database-chatting-app-a928494373d5.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
